5ac80e485b7fb3f6d1641c52b0af62d842180de3
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-07-04 21:44:22 +0100

    Add project files.

---
+++ MyVeryFirstMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyVeryFirstMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MyVeryFirstMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVeryFirstMauiApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
